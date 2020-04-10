f083a14061264e7e7d47505239d6bcbc58c1f761
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-10 12:55:37 +0530

    :art: Add settings for DRF

--- home/settings.py
+++ home/settings.py
@@ -83,3 +83,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
