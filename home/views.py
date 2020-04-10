d8cef48410f7abf8d23b61205b978ab8732cea41
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-10 18:36:23 +0530

    :sparkles: Add views and templates

---
+++ home/views.py
@@ -0,0 +1,54 @@
+from django.views.generic import CreateView, DeleteView, UpdateView, DetailView, TemplateView
+from .models import Bulb, Fan
+
+
+class BulbCreateView(CreateView):
+    model = Bulb
+    template_name = 'bulb/create.html'
+    fields = ('brightness', 'color', 'owner', 'name', 'state')
+
+
+class BulbDeleteView(DeleteView):
+    model = Bulb
+
+
+class BulbUpdateView(UpdateView):
+    model = Bulb
+    template_name = 'bulb/update.html'
+    fields = ('brightness', 'color', 'owner', 'name', 'state')
+
+
+class BulbDetailView(DetailView):
+    model = Bulb
+    template_name = 'bulb/create.html'
+
+
+class FanCreateView(CreateView):
+    model = Fan
+    template_name = 'fan/create.html'
+    fields = ('speed', 'owner', 'name', 'state')
+
+
+class FanDeleteView(DeleteView):
+    model = Fan
+
+
+class FanUpdateView(UpdateView):
+    model = Fan
+    template_name = 'fan/update.html'
+    fields = ('speed', 'owner', 'name', 'state')
+
+
+class FanDetailView(DetailView):
+    model = Fan
+    template_name = 'fan/detail.html.html'
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = {
+
+        }
+        return ctx
