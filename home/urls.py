032b1b5cb830faa5cd55a2c8b3b9c61916f97746
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-11 17:39:39 +0530

    :art: Move url entry to independant file

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,6 @@ from django.urls import include, path
 from django.contrib import admin
 from rest_framework import routers
 
-from django.contrib.auth import views as auth_views
-
 from .views import BulbCreateView, FanCreateView, BulbDeleteView, FanDeleteView, BulbUpdateView, FanUpdateView, \
     BulbDetailView, FanDetailView, Home
 from .viewsets import BulbViewSet, FanViewSet
@@ -15,9 +13,8 @@ router.register(r'bulb', BulbViewSet)
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('admin', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('login', auth_views.LoginView.as_view(), {'template_name':'login.html', 'success_url': ''}, name='login'),
-    path('logout', auth_views.LogoutView.as_view(), {'template_name':'logout.html'}, name='logout'),
+    path('api', include(router.urls)),
+    path('accounts/', include('home.accounts_urls'), name='accounts'),
     path('bulb/create', BulbCreateView.as_view(), name='bulb-create'),
     path('bulb/update/<pk>/', BulbUpdateView.as_view(), name='bulb-update'),
     path('bulb/delete/<pk>/', BulbDeleteView.as_view(), name='bulb-delete'),
