ac3b3605b681caedff18b30cf9418bb5a21a1b74
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-11 20:32:54 +0530

    :art: Add profile url entry

--- home/accounts_urls.py
+++ home/accounts_urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import views as auth_views
 from django.urls import  path
 
+from home.views import Home
+
 app_name='accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), {'template_name': 'login.html', 'success_url': ''}, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), {'template_name': 'logout.html'}, name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('profile/', Home.as_view()),
 ]
