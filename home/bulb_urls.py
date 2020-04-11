57f0576d34af1bd205d99cd2640afac689d26d72
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-11 19:24:32 +0530

    :art: Add namespace and remove prefix

--- home/bulb_urls.py
+++ home/bulb_urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from home.views import BulbCreateView, BulbUpdateView, BulbDeleteView, BulbDetailView
 
+app_name='bulb'
 urlpatterns = [
-    path('create/', BulbCreateView.as_view(), name='bulb-create'),
-    path('update/<pk>/', BulbUpdateView.as_view(), name='bulb-update'),
-    path('delete/<pk>/', BulbDeleteView.as_view(), name='bulb-delete'),
-    path('detail/<pk>/', BulbDetailView.as_view(), name='bulb-detail'),
+    path('create/', BulbCreateView.as_view(), name='create'),
+    path('update/<pk>/', BulbUpdateView.as_view(), name='update'),
+    path('delete/<pk>/', BulbDeleteView.as_view(), name='delete'),
+    path('detail/<pk>/', BulbDetailView.as_view(), name='detail'),
 ]
--- home/fan_urls.py
+++ home/fan_urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from home.views import FanCreateView, FanDeleteView, FanUpdateView, FanDetailView
 
+app_name='fan'
 urlpatterns = [
-    path('create/', FanCreateView.as_view(), name='fan-create'),
-    path('delete/<pk>/', FanDeleteView.as_view(), name='fan-delete'),
-    path('update/<pk>/', FanUpdateView.as_view(), name='fan-update'),
-    path('detail/<pk>/', FanDetailView.as_view(), name='bulb-detail'),
+    path('create/', FanCreateView.as_view(), name='create'),
+    path('delete/<pk>/', FanDeleteView.as_view(), name='delete'),
+    path('update/<pk>/', FanUpdateView.as_view(), name='update'),
+    path('detail/<pk>/', FanDetailView.as_view(), name='detail'),
 ]
