131120386db52558a543f9a6fe667d87032e37ff
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-11 18:04:32 +0530

    :sparkles: Add authentication views

---
+++ home/accounts_urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import views as auth_views
+from django.urls import  path
+
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), {'template_name': 'login.html', 'success_url': ''}, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), {'template_name': 'logout.html'}, name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
