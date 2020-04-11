17603146da9e355e39af4e5a03d6ba575812536a
Ajay Katwe <ajay.katwe@gmail.com>
2020-04-11 17:40:17 +0530

    :art: Update mixin import Order

--- home/views.py
+++ home/views.py
@@ -3,49 +3,49 @@ from django.views.generic import CreateView, DeleteView, UpdateView, DetailView,
 from .models import Bulb, Fan
 
 
-class BulbCreateView(CreateView, LoginRequiredMixin):
+class BulbCreateView(LoginRequiredMixin, CreateView):
     model = Bulb
     template_name = 'bulb/create.html'
     fields = ('brightness', 'color', 'owner', 'name', 'state')
 
 
-class BulbDeleteView(DeleteView, LoginRequiredMixin):
+class BulbDeleteView(LoginRequiredMixin, DeleteView):
     model = Bulb
 
 
-class BulbUpdateView(UpdateView, LoginRequiredMixin):
+class BulbUpdateView(LoginRequiredMixin, UpdateView):
     model = Bulb
     template_name = 'bulb/update.html'
     fields = ('brightness', 'color', 'owner', 'name', 'state')
 
 
-class BulbDetailView(DetailView, LoginRequiredMixin):
+class BulbDetailView(LoginRequiredMixin, DetailView):
     model = Bulb
     template_name = 'bulb/detail.html'
 
 
-class FanCreateView(CreateView, LoginRequiredMixin):
+class FanCreateView(LoginRequiredMixin, CreateView):
     model = Fan
     template_name = 'fan/create.html'
     fields = ('speed', 'owner', 'name', 'state')
 
 
-class FanDeleteView(DeleteView, LoginRequiredMixin):
+class FanDeleteView(LoginRequiredMixin, DeleteView):
     model = Fan
 
 
-class FanUpdateView(UpdateView, LoginRequiredMixin):
+class FanUpdateView(LoginRequiredMixin, UpdateView):
     model = Fan
     template_name = 'fan/update.html'
     fields = ('speed', 'owner', 'name', 'state')
 
 
-class FanDetailView(DetailView, LoginRequiredMixin):
+class FanDetailView(LoginRequiredMixin, DetailView):
     model = Fan
     template_name = 'fan/detail.html'
 
 
-class Home(TemplateView, LoginRequiredMixin):
+class Home(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
