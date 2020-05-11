102959de649fb700ce32d56140ade5636fe96bcd
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-11 18:31:00 +0530

    :art: Update serializers to add link

--- home/serializiers.py
+++ home/serializiers.py
@@ -6,10 +6,10 @@ from rest_framework.serializers import ModelSerializer
 class BulbSerializer(ModelSerializer):
     class Meta:
         model = Bulb
-        fields = ('id','owner', 'name', 'state', 'brightness', 'color')
+        fields = ('id','owner', 'name', 'state', 'brightness', 'color', 'get_absolute_url')
 
 
 class FanSerializer(ModelSerializer):
     class Meta:
         model = Fan
-        fields = ('id', 'owner', 'name', 'state', 'speed')
+        fields = ('id', 'owner', 'name', 'state', 'speed', 'get_absolute_url')
